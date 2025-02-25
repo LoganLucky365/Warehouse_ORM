923065ad9a4643d14859626952d69b44128aba8f
LoganLucky365 <sebastian.profous@outlook.com>
2025-02-25 14:29:43 +0100

    Initial Commit

---
+++ src/main/java/org/example/warehouse_orm/WarehouseOrmApplication.java
@@ -0,0 +1,13 @@
+package org.example.warehouse_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseOrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/warehouse_orm/WarehouseOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.warehouse_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
